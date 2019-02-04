b87bbc9d6f58710766c90a14e232d366939411dd
ErwannGui <erwann.guillevic@ynov.com>
2019-02-04 14:51:19 +0100

    remove unused dependancy + login page

--- src/components/components.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { BottomBannerComponent } from './bottom-banner/bottom-banner';
-@NgModule({
-	declarations: [BottomBannerComponent],
-	imports: [],
-	exports: [BottomBannerComponent]
-})
-export class ComponentsModule {}
